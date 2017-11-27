637f1d090e34e1d9361a6d1ad1511d1ffe3b5e8e
AlexThomson9 <1507262@uhi.ac.uk>
2017-11-27 15:58:11 +0000

    Updated Register Login Works

--- app/src/main/java/com/example/alex/groupprojectcomplete/DatabaseUtility.java
+++ app/src/main/java/com/example/alex/groupprojectcomplete/DatabaseUtility.java
@@ -27,29 +27,21 @@ public class DatabaseUtility {
         //  myDB = db.getReadableDatabase();
 
     }
-
     public ArrayList<String> getTextbookviaModule() {
 
         List<String> textbookviaModule = new ArrayList<String>();
         Cursor cursor = null;
         String countQuery = "SELECT * FROM " + DB_NAME + " CROSS JOIN Selling WHERE module = '"+ BuySearch.ModuleNumber + "' ";
         try {
-
             cursor = myDB.rawQuery(countQuery, null);
-
         }catch (SQLException e){
-
-
             e.printStackTrace();
         }while(cursor.moveToNext()){
 
             textbookviaModule.add(cursor.getString(1));
             textbookviaModule.add(cursor.getString(2));
             textbookviaModule.add(cursor.getString(7));
-
-
         }  cursor.close();
-
 // return count
         return (ArrayList<String>)textbookviaModule;
     }
@@ -84,17 +76,10 @@ public class DatabaseUtility {
 
         try {
             cur = myDB.rawQuery(Qry, null);
-
-
-
         }catch (SQLException e){
-
             e.printStackTrace();
-
         }
         while (!cur.isAfterLast()){
-
-
          /*   book.add(cur.getString(1));
             book.add(cur.getString(2));
             book.add(cur.getString(3));
@@ -102,24 +87,13 @@ public class DatabaseUtility {
             book.add(cur.getString(5));
             book.add(cur.getString(6));
             book.add(cur.getString(7));
-
 */
-
-
          book.add(cur.getString(0));
             cur.moveToNext();
-
-
-
         }
-
         return (ArrayList<String>)book;
-
-
-
     }
-
-
+    public String BookNumber;
 
     public ArrayList<String> validate(){
         List<String> part1 = new ArrayList<String>();
@@ -140,11 +114,13 @@ public class DatabaseUtility {
             while(curs.moveToNext())
             {
                 part1.add(curs.getString(0));
+                BookNumber = part1.get(0);
             }
            // curs.close();
             if(part1.get(0) != null)
             {
                 String ISBN = part1.get(0);
+                part1.clear();
                 String qry2 = "SELECT uID FROM Selling WHERE  ISBN =  '" + ISBN + "'" ;
                 try
                 {
@@ -156,7 +132,7 @@ public class DatabaseUtility {
                 }
                 while(curs.moveToNext())
                 {
-
+                    part2.clear();
                     part2.add(curs.getString(0));
                 }
             }
@@ -176,11 +152,14 @@ public class DatabaseUtility {
             while(curs.moveToNext())
             {
                 part1.add(curs.getString(0));
+                BookNumber = part1.get(0);
+
             }
             curs.close();
             if(part1.get(0) != null)
             {
                 String ISBN = part1.get(0);
+                part1.clear();
                 String qry2 = "SELECT uID FROM Selling WHERE  ISBN =  '" + ISBN + "'" ;
                 try
                 {
@@ -196,24 +175,89 @@ public class DatabaseUtility {
                     part2.add(curs.getString(0));
                 }
             }
-
-
-
-
         }
-
-
-
-
-
-
-
-
         return (ArrayList<String>)part2;
     }
 
+    public ArrayList<String> getSellerPart1(){
+        List<String> seller = new ArrayList<String>();
+        Cursor get = null;
+
+
+        String userID = validate().get(0);
+        String sql = "SELECT * FROM Selling WHERE uID = '" + userID + "'";
+
+        try{
+
+            get = myDB.rawQuery(sql, null);
+
+        }catch (SQLException e){
+
+
+            e.printStackTrace();
+        }while(get.moveToNext()){
+
+
+            seller.add(get.getString(0));
+            seller.add(get.getString(1));
+            seller.add(get.getString(2));
+            seller.add(get.getString(3));
+        }
+
+            return (ArrayList<String>)seller;
+    }
+
+
+    public ArrayList<String> getSellerPart2(){
+        List<String> info = new ArrayList<String>();
+        Cursor cur = null;
+
+        String ISBN = getSellerPart1().get(1);
+        String sql = "SELECT name, Module FROM BookInfo WHERE ISBN = '" + ISBN + "'";
+
+
+        try{
+
+            cur = myDB.rawQuery(sql, null);
+
+
+        }catch (SQLException e){
 
 
 
+            e.printStackTrace();
+        }while(cur.moveToNext()){
+            info.add(cur.getString(0));
+            info.add(cur.getString(1));
+
+        }
+        return (ArrayList<String>)info;
+    }
+
+    public ArrayList<String> getLink(){
+        List<String> link = new ArrayList<String>();
+        Cursor cur = null;
+
+        String sql = "SELECT name, module , link FROM BookInfo CROSS JOIN Selling WHERE ISBN =  '" + BookNumber + "'";
+        try{
+
+
+            cur = myDB.rawQuery(sql, null);
+
+        }catch (SQLException e){
+
+
+
+            e.printStackTrace();
+        }while(cur.moveToNext()){
+
+            link.add(cur.getString(0));
+            link.add(cur.getString(1));
+            link.add(cur.getString(3));
+
+
+        }
+        return (ArrayList<String>)link;
+    }
 
 }
