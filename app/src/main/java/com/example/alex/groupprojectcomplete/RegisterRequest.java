d21afb1c312c99bb09a1c3a2125da9901a7a6449
AlexThomson9 <1507262@uhi.ac.uk>
2017-11-27 12:37:29 +0000

    Updated Register Login Works

--- app/src/main/java/com/example/alex/groupprojectcomplete/RegisterRequest.java
+++ app/src/main/java/com/example/alex/groupprojectcomplete/RegisterRequest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class RegisterRequest extends StringRequest{
 
-    private static final String REGISTER_REQUEST_URL = "https://files.000webhostapp.com/Register.php";
+    private static final String REGISTER_REQUEST_URL = "http://textbookapp.000webhostapp.com/Register.php";
 
     private Map<String, String>params;
     public RegisterRequest(String name, String username, String password, Response.Listener<String> listener){
