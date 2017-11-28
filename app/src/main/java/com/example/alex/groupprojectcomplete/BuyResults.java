40f74b8f3365e6ddc4fa0db618c75fd287474f35
AlexThomson9 <1507262@uhi.ac.uk>
2017-11-28 12:42:51 +0000

    Made it look nice :P

--- app/src/main/java/com/example/alex/groupprojectcomplete/BuyResults.java
+++ app/src/main/java/com/example/alex/groupprojectcomplete/BuyResults.java
@@ -1,5 +1,6 @@
 package com.example.alex.groupprojectcomplete;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +16,84 @@ public class BuyResults extends AppCompatActivity implements View.OnClickListene
         databaseStuff = new DatabaseUtility(this);
         TextView text;
 
-        text = (TextView)this.findViewById(R.id.textView4);
+        text = (TextView)this.findViewById(R.id.textView19) ;
 
-       // text.setText(databaseStuff.getTextbook().toString());
+        ConstraintLayout Result1 = (ConstraintLayout)this.findViewById(R.id.Result1);
+        ConstraintLayout Result2 = (ConstraintLayout)this.findViewById(R.id.Result2);
+        ConstraintLayout Result3 = (ConstraintLayout)this.findViewById(R.id.Result3);
+        View line1 = (View)this.findViewById(R.id.view5);
+        View line2 = (View)this.findViewById(R.id.view7);
+
+
+        Result1.setVisibility(View.INVISIBLE);
+        Result2.setVisibility(View.INVISIBLE);
+        Result3.setVisibility(View.INVISIBLE);
+        line1.setVisibility(View.INVISIBLE);
+        line2.setVisibility(View.INVISIBLE);
+
+
+        TextView Title = (TextView)this.findViewById(R.id.c1Title);
+        TextView Condition = (TextView)this.findViewById(R.id.c1Condition);
+        TextView Price = (TextView)this.findViewById(R.id.c1Price);
+        TextView P_Title = (TextView)this.findViewById(R.id.c1PriceTitle);
+        TextView C_Title = (TextView)this.findViewById(R.id.c1ConditionTitle);
+
+
+
+
+        if(databaseStuff.validate().get(0) == null)
+        {
+            if (BuySearch.ModuleNumber.length() != 0 )
+            {
+                int Tvm = databaseStuff.getTextbookviaModule().size();
+                String test1 = String.valueOf(Tvm);
+                Log.d("meme",test1 );
+               text.setText(databaseStuff.getLink().toString());
+
+                Log.d("dank", databaseStuff.BookNumber);
+
+            }
+            else if(BuySearch.BookName.length() != 0 )
+            {
+                int Tvn = databaseStuff.getTextbookviaName().size();
+                String test2 = String.valueOf(Tvn);
+                Log.d("memee",test2 );
+
+                if (Tvn <= 6){
+
+
+                    String memes = databaseStuff.getLink().toString();
+                    Log.d("guess", memes);
+                    Result1.setVisibility(View.VISIBLE);
+
+                    C_Title.setText("Module Number");
+                    P_Title.setText("Link to Purchaser");
+                    Title.setText(databaseStuff.getLink().get(0).toString());
+                    Condition.setText(databaseStuff.getLink().get(1).toString());
+                    Price.setText(databaseStuff.getLink().get(2).toString());
+
+                }
+
+              //  text.setText(databaseStuff.getLink().toString());
+
+
+
+
+            }
+        }
+        else if (databaseStuff.validate().get(0) != null)
+        {
+            int lnk = databaseStuff.getLink().size();
+            String test3 = String.valueOf(lnk);
+            Log.d("meme33",test3 );
+        }
+
+
+
+
 
         TextView book;
-        book = (TextView)this.findViewById(R.id.textView5);
+        //book = (TextView)this.findViewById(R.id.textView5);
        // book.setText(databaseStuff.getSeller().toString());
         // user databaseStuff.getSeller().get(1) to get 2nd value and so forth
 
@@ -29,7 +102,7 @@ public class BuyResults extends AppCompatActivity implements View.OnClickListene
           //  String test = databaseStuff.validate().toString();
 
         Log.d ("aaaaa", databaseStuff.validate().toString());
-       if(databaseStuff.validate().get(0) == null){
+      /* if(databaseStuff.validate().get(0) == null){
 
           //  book.setText(databaseStuff.getSeller().toString());
             text.setText(databaseStuff.getTextbookviaModule().get(0));
@@ -52,7 +125,7 @@ public class BuyResults extends AppCompatActivity implements View.OnClickListene
            book.setText(databaseStuff.validate().toString());
          //  book.setText(instock);
        }
-
+*/
 
 
     }
